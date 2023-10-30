dc9c279509414fa4fec581082ed9191c9c52d350
Astoner <aidanstoner1@gmail.com>
2023-10-30 19:06:04 -0400

    updated header

--- Final_Pt1.c
+++ Final_Pt1.c
@@ -1,7 +1,9 @@
-//Name:
-//Date:
-//Course:
-//Description:
+//Name:Aidan Stoner, Marc Santacapita, Shaunessy Reynolds, and Erin Cardino.
+//Date:11/6/2023
+//Course:ELEC-3371
+//Description: This file will contain two functions, one to read the value from our
+//             joystick and return someothing in a generic way. The other needs
+//             read an analog potentiometer value.
 //******************************************************************************
 void main() {
 
