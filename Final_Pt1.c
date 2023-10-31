08e068ddba22cfe5f0d08fbcdbe40cf654bf7566
Astoner <aidanstoner1@gmail.com>
2023-10-30 20:13:20 -0400

    fixed initializations and added function

--- Final_Pt1.c
+++ Final_Pt1.c
@@ -8,6 +8,9 @@
 //Global Variables:
 
 //******************************************************************************
+//My Functions:
+int joyRead();
+//******************************************************************************
 //Main Function:
 void main() {
 //Initialization:
@@ -17,13 +20,14 @@ void main() {
      RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
      RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
      
-     GPIOA_CRL = 0x00400000; //Sets PA6 as an input
-     GPIOB_CRL = 0x00040000; //sets PB5 as an input
-     GPIOB_CRH = 0x00400000; //sets PC13 as an input
-     GPIOD_CRL = 0x00004040; //Set PD2 and PD4 as an input *as an FYI we
+     GPIOA_CRL = 0x44444444; //Sets PortA/L as an input
+     GPIOB_CRL = 0x44444444; //sets PortB/L as an input
+     GPIOC_CRH = 0x44444444; //sets PortC/H as an input
+     GPIOD_CRL = 0x44444444; //Sets PortD/L as an input *as an FYI we
           //may have to change this later to be wihtin the function as for
           //obj 3 all of portD needs to be an ouput.
      GPIOE_CRH = 0x33333333; //Set PortE/H as an output for LEDS
 //******************************************************************************
-
+     joyRead();
+     
 }
